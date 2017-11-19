8cde0300c9718bdc270ce274db880ec789170e14
PE4\genna <gennadiy082@gmail.com>
2017-11-19 20:08:09 +0300

    [son] v.0.0.1 add provider and logger

---
+++ Temporary-Prison/Temporary-Prison.Business/Provider/IPrisonProvider.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Temporary_Prison.Business
+{
+    public interface IPrisonProvider
+    {
+        string GetMessage();
+    }
+}
---
+++ Temporary-Prison/Temporary-Prison.Business/Provider/PrisonProvider.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Temporary_Prison.Business
+{
+    public class PrisonProvider : IPrisonProvider
+    {
+        public string GetMessage()
+        {
+            return "Test provider";
+        }
+    }
+}
